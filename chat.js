df0e1ac69a2cb0890d1f7598e4d4fdded269e4cc
Christian Alexander Debruin <cad11@linux67.macs.hw.ac.uk>
2019-10-03 15:28:02 +0100

    did some work on chat feature

---
+++ chat.js
@@ -0,0 +1,19 @@
+// Nonfunctional Mock Up
+
+function wizardOfOz() {
+    var newMsg = document.createElement("div");
+    newMsg.innerHTML = "Hi";
+    document.body.appendChild(newMsg);
+}
+
+
+//Post Request to Chat Database
+
+// TODO: Collect UserName, TimeStamp, Message
+// TODO: Post Above to Database
+// TODO: Clear Message from form
+
+//Get Request to check for new messages
+
+// TODO: send TimeStamp with query. User Joined is initial TimeStamp
+// TODO: return new messages with TimeStamp and UserName and append to local chat log
