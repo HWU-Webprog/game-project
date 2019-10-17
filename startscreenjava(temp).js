c33cb21b52aa2799ea24472931ee8428c9a33b3e
Cameron <55835365+RedCJB15@users.noreply.github.com>
2019-10-17 13:24:21 +0100

    Update and rename startscripts.js to startscreenjava(temp).js

--- startscreenjava(temp).js
+++ startscreenjava(temp).js
@@ -3,3 +3,4 @@ function createplayer() {
   var Colour = document.getElementById('COLOUR').value
   alert("Username: " + Username + "\nColour: " + Colour);
 }
+// This file is tempoary, it shows how to recieve the username and colour from the start screen
