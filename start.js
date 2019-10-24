16698622a6c5e42722de23d1e225973308128fa4
Calum Shepherd <calum.shep@icloud.com>
2019-10-24 12:08:50 +0100

    reformatted and adjusted comments

--- start.js
+++ start.js
@@ -1,14 +1,16 @@
-/*
-This file is for holding the functions used to set up many other
-elements of the site.
-*/
+/**
+ * This file is for holding the functions used to set up many other
+ * elements of the site.
+ */
 
-// Draw the canvas and draw a player object
+/**
+ * Function to draw the canvas and draw a player object
+ */
 function start() {
-  canvas.start();
-  var testplayer = new Player("TestName", "blue", 50, 50);
-  testplayer.draw();
+    canvas.start();
+    var testplayer = new Player("TestName", "blue", 50, 50);
+    testplayer.draw();
 
-  // See drawLine call example below.
-  drawLine(0,0,100,100)
+    // See drawLine call example below.
+    drawLine(0,0,100,100)
 }
