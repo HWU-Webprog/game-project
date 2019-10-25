92f0f01c8688b1e60028b096a0a666f5110ab1b3
Calum Shepherd <calum.shep@icloud.com>
2019-10-25 10:17:09 +0100

    beefed up script to be more like a migration

--- database/provision-database.php
+++ database/provision-database.php
@@ -1,7 +1,46 @@
 <?php
+
+/**
+ * Database provisioning script
+ * @author Calum Shepherd
+ *
+ * GET request this page as follows to run the script:
+ *      .../provision-database.php?type=
+ *          up      to create tables
+ *          down    to delete tables
+ */
+
 $db = new SQLite3('database.sqlite', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
-$db->exec('CREATE TABLE IF NOT EXISTS "users"(
-    "id" INTEGER PRIMARY KEY,
-    "name" TEXT
-)');
+if (isset($_GET['type']))
+{
+    switch ($_GET['type'])
+    {
+        case "up":
+            try {
+                $db->exec('CREATE TABLE IF NOT EXISTS users(
+                    "id" INTEGER PRIMARY KEY,
+                    "username" TEXT,
+                    "name" TEXT,
+                    "password" TEXT
+                )');
+                // insert test user
+                $stmt = $db->prepare('INSERT INTO users("username","name","password") VALUES(:username,:name,:password');
+                $user = [
+                    'username'  => 'test',
+                    'name'      => 'Test User',
+                    'password'  => password_hash('password', PASSWORD_DEFAULT)
+                ];
+                foreach ($user as $property) $stmt->bindValue(':'.$property, $user[$property]);
+            } catch (Exception $e) { echo $e->getMessage(); }
+            break;
+
+        case "down":
+            $db->exec('DROP TABLE IF EXISTS "users"');
+            break;
+
+        default:
+            echo 'That is not a valid provision type. Try "up" or "down".';
+    }
+}
+else echo 'No provision type set. Append "?type=" and then either "up" or "down" to the page URL';
