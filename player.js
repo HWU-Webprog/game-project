8506076992684ff30b08bff54ed6bbb14a84df8f
Kyle <kd41@hw.ac.uk>
2019-11-07 15:50:19 +0000

    Movement implemented using velocity for consistant speed, wrapping also implemented

--- player.js
+++ player.js
@@ -3,6 +3,19 @@
  * object that is drawn to the canvas
  */
 
+// enum for direction
+const direction = {
+    N: 'north',
+    NE: 'north east',
+    E: 'east',
+    SE: 'south east',
+    S: 'south',
+    SW: 'south west',
+    W: 'west',
+    NW: 'north west',
+    DEFAULT: 'stationary'
+}
+
 /**
  * Constructor for new Player object
  *
@@ -13,13 +26,13 @@
  * @param      {int}  y         Starting Y coord
  */
 function Player(name, color, x, y) {
-    this.speedX = 0;
-    this.speedY = 0;
     this.name = name;
     this.color = color;
     this.x = x;
     this.y = y;
-    this.onCooldown = false;
+    this.velocityX = 0;
+    this.velocityY = 0;
+    this.boostActive = false;
 
     /**
      * Draws Player object on canvas
@@ -29,35 +42,154 @@ function Player(name, color, x, y) {
         player = canvas.context;
         // Set the color of the object to player chosen color
         player.fillStyle = color;
-        // Create a 20x20 square at the provided position
 
+        // Move object relative to velocity
+        this.x += this.velocityX;
+        this.y += this.velocityY;
+
+        this.checkBounds();
+        
+    
+        // Create a 20x20 square at the provided position
         player.fillRect(this.x,this.y,20,20);
     },
-    this.moveLeft = function(){
-        this.x -= this.speedX;
-    },
-    this.moveRight = function(){
-        this.x += this.speedX;
-    },
-    this.moveUp = function(){
-        this.y -= this.speedY;
-    },
-    this.moveDown = function(){
-        this.y += this.speedY;
-    },
-    this.resetspeed = function(){
-        this.speedX = 0;
-        this.speedY = 0;
-        this.onCooldown = false;
-    },
-    this.dash = function(){
-        if (this.onCooldown == false) {
-            this.speedX = 2*this.speedX;
-            this.speedy = 2*this.speedy;
+    this.checkBounds = function(){
+        if(this.y > 500){
+            this.y = 0;
+        } else if (this.y < -20){
+            this.y = 500;
+        }
 
-            this.onCooldown = true;
+        if(this.x > 500){
+            this.x = 0;
+        } else if (this.x < -20){
+            this.x = 500;
+        }
+    },
+    this.boost = function(){
 
-            setTimeout(this.resetspeed, 2000);
+    },
+    this.noboost = function(){
+        
+    }
+}
+
+function moveLeft(){
+    testplayer.velocityX -= 0.5; 
+}
+
+function moveRight(){
+    testplayer.velocityX += 0.5;
+}
+
+function moveUp() {
+    testplayer.velocityY -= 0.5;
+}
+
+function moveDown(){
+    testplayer.velocityY += 0.5;
+}
+
+function resetspeed(){
+    testplayer.speedX = 0;
+    testplayer.speedY = 0;
+    testplayer.onCooldown = false;
+}
+
+function dash(){
+    var prevX = testplayer.velocityX;
+    var prevX = testplayer.velocityY;
+
+    testplayer.boostActive = true;
+    
+    var currentDirection = getDirection();
+    console.log(currentDirection);
+
+    switch(currentDirection){
+        case "north":
+            testplayer.velocityY -= 10;
+            break;
+        case "north east":
+            testplayer.velocityY -= 10;
+            testplayer.velocityX += 10;
+            break;
+        case "east":
+            testplayer.velocityX += 10;
+            break;
+        case "south east":
+            testplayer.velocityY += 10;
+            testplayer.velocityX += 10;
+            break;
+        case "south":
+            testplayer.velocityY += 10;
+            break;
+        case "south west":
+            testplayer.velocityY += 10;
+            testplayer.velocityX -= 10;
+            break;
+        case "west":
+            testplayer.velocityX -= 10;
+            break;
+        case "north west":
+            testplayer.velocityY -= 10;
+            testplayer.velocityX -= 10;
+            break;
+        default:
+            break;
+    }
+}
+
+function getDirection(){
+    var velX = testplayer.velocityX;
+    var velY = testplayer.velocityY;
+
+    if(velY < 0){
+        if(velX == 0){
+            // moving north
+            return direction.N;
+        } else if(velX > 0){
+            // moving north east
+            return direction.NE;
+        } else if(velX < 0){
+            // moving north west
+            return direction.NW;
+        }
+    } else if (velY > 0){
+        if (velX == 0){
+            // moving south
+            return direction.S;
+        } else if (velX > 0){
+            // moving south east
+            return direction.SE;
+        } else if (velX < 0){
+            // moving south west
+            return direction.SW;
+        }
+    } else if (velY == 0){
+        if (velX > 0){
+            // moving east
+            return direction.E;
+        } else if (velX < 0){
+            // moving west
+            return direction.W;
+        } else if (velX == 0){
+            return direction.DEFAULT;
         }
     }
 }
+
+$(document).keydown(function(e) {
+    if(e.which == 37)
+        moveLeft();
+
+    if(e.which == 38)
+        moveUp();
+        
+    if(e.which == 39)
+        moveRight();
+    
+    if(e.which == 40)
+        moveDown();
+    if(e.which == 81)
+        dash();
+})
--- start.js
+++ start.js
@@ -3,22 +3,20 @@
 * This file is for holding the functions used to set up many other
 * elements of the site.
 */
-var testplayer
-
+var testplayer;
 
 /**
 * Function to draw the canvas and draw a player object
 */
 function start() {
-    canvas.start();
     // Calls the Username and Colour from the Starts screen text box and drop down menu
-    var testplayer = new Player(document.getElementById('USERNAME').value, document.getElementById('COLOUR').value, 50, 50);
-    testplayer.draw();
+    //var testplayer = new Player(document.getElementById('USERNAME').value, document.getElementById('COLOUR').value, 50, 50);
+    testplayer = new Player("Test", "#FF0000", 50, 50);
+    canvas.start();
 }
 
 // Updates the canvas every 20 ms
 function update(){
     canvas.clear();
-    testplayer.y -= 1;
     testplayer.draw();
 }
