decfeb9cfb5408144633f7c1417b13feb5d97301
MagnificentBubble <cad11@hw.ac.uk>
2019-11-07 16:46:30 +0000

    Implemented plan for collision.js

---
+++ assets/js/collision.js
@@ -0,0 +1,17 @@
+/**
+ * INSERT COLLISIONS HERE
+ * 
+ * BOUNCE
+ * Add Velocities Together
+ * Assign this as new velocity for each player in Random Opposite Directions
+ * 
+ * KILL
+ * Muder the player not dashing
+ * Reset Cooldown?
+ * 
+ * DETECT COLLISION
+ * If one player XOR the other is Dashing
+ * KILL
+ * else
+ * BOUNCE
+ */
