959ec955ba68fcc4a7aaef52f55e7b7ae9419574
Calum Shepherd <calum.shep@icloud.com>
2019-11-14 13:27:07 +0000

    added top navbar

--- assets/layout/header.php
+++ assets/layout/header.php
@@ -12,3 +12,18 @@
 </head>
 
 <body>
+
+    <div class="header">
+        <p class="menu-left">
+            Firehaus
+        </p>
+        <p class="menu-right">
+            <a class="nav-link" href="<?= App\Config::DOMAIN ?>/auth/user/login.php">
+                <i class="fas fa-fw fa-key icon-padding-right"></i> Login
+            </a>
+            &middot;
+            <a class="nav-link" href="<?= App\Config::DOMAIN ?>/auth/user/register.php">
+                <i class="fas fa-fw fa-user-plus icon-padding-right"></i> Register
+            </a>
+        </p>
+    </div>
