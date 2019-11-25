d478c6570c410f857d59ed5239ca05843fed5e55
Kyle <kd41@hw.ac.uk>
2019-11-25 10:19:26 +0000

    username and colour now reflects in player creation

--- start.js
+++ start.js
@@ -5,21 +5,17 @@
 var testplayer
 
 // When the join game button is pressed, the menu is hid
-$(document).ready(function(){
+$(document).ready( () => {
     $("#joinGame").click(function(){
         $("#main").hide();
+        var username = $("#USERNAME").val();
+        var color = $("#COLOUR").val();
+        testplayer = new Player(username, color, 50, 50);
+        canvas.start();
+        // Calls the Username and Colour from the Starts screen text box and drop down menu (right now just holds test variables)
+        testplayer.draw();
     })
 })
-
-/**
- * Function to draw the canvas and draw a player object
- */
-function start() {
-    canvas.start();
-    // Calls the Username and Colour from the Starts screen text box and drop down menu (right now just holds test variables)
-    testplayer = new Player("test","RED", 50, 50);
-    testplayer.draw();
-}
 
 // Updates the canvas every 20 ms
 function update() {
