4d6e5ee302a094e7231a5b88ee697cde9122e891
Calum Shepherd <calum.shep@icloud.com>
2019-11-25 10:01:46 +0000

    reformatted output to suit bash script

--- app/SQLiteProvision.php
+++ app/SQLiteProvision.php
@@ -50,7 +50,6 @@ class SQLiteProvision
     public function insertTestData()
     {
         // test users
-        echo 'INSERT INTO users("username","name","password","bio") VALUES("testUser","Test User","'.password_hash('test', PASSWORD_DEFAULT).'", "Nulla excepteur ut labore do irure officia ut nisi excepteur sed mollit sed.")';
         $this->pdo->exec('INSERT INTO users("username","name","password", "bio") VALUES("testUser","Test User","'.password_hash('test', PASSWORD_DEFAULT).'", "Nulla excepteur ut labore do irure officia ut nisi excepteur sed mollit sed.")');
         $this->pdo->exec('INSERT INTO users("username","name","password", "bio") VALUES("testUser2","Test User2","'.password_hash('test2', PASSWORD_DEFAULT).'", "Enim exercitation magna dolore irure aliqua sit amet commodo eu proident veniam do minim labore dolor nulla voluptate irure esse mollit.")');
     }
@@ -72,7 +71,7 @@ class SQLiteProvision
         {
             $this->createTables();
             $this->insertTestData();
-            echo 'Database provisioned up!';
+            echo 'Database provisioned up!\n';
         }
         catch (Exception $e)
         {
@@ -86,6 +85,6 @@ class SQLiteProvision
     public function down()
     {
         $this->dropTables();
-        echo 'Database provisioned down!';
+        echo 'Database provisioned down!\n';
     }
 }
